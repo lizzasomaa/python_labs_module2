4325347c72bae0ebe19bf6a43a0d7abc36518dee
lizzasomaa <lizzasomaa@yahoo.com>
2025-02-04 22:38:57 +0300

    Add files via upload

---
+++ Лабораторная 3/lab3.py
@@ -0,0 +1,67 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self.name = name
+        self.author = author
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def author(self):
+        return self._author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook:
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.pages = pages
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, new_pages):
+        if not isinstance(new_pages, int):
+            raise TypeError("Количество страниц должно быть типа int")
+        if new_pages <= 0:
+            raise ValueError("Количество страниц должно быть положительным числом")
+        self._pages = new_pages
+
+    def __str__(self):
+        return f"Бумажная книга {self.name}. Автор {self.author}. Страниц: {self.pages}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages})"
+
+
+class AudioBook:
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, new_duration: float):
+        if not isinstance(new_duration, int):
+            raise TypeError("Продолжительность должна быть типа float")
+        if new_duration <= 0:
+            raise ValueError("Длительность должна быть положительным числом")
+        self._duration = new_duration
+
+    def __str__(self):
+        return f"Аудиокнига {self.name}. Автор {self.author}. Длительность: {self.duration} часов"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration})"
